ba4d8c28de5fe1f73ddc1832939948aa681efa45
jcordes <jjcordes@ca.rr.com>
2015-05-20 11:16:41 -0700

    Assignment #2
    Returns a matrix that is the inverse of a provided matrix, but only if
    it hasn't already been cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This gets called first with the matrix in question
+#to provide caching functionality.
+#Usage...  "Magic = makeCacheMatrix(B)
+#           cacheSolve(Magic)"
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(originalMatrix = matrix()) { 
+  #Clear out the existing inverse matrix, there's going to be a new one
+  theInverseMatrix <- NULL 
+  
+  #Once the magic function list is created, the "set" function can be used
+  #to change the original matrix
+  set <- function(y) {
+    originalMatrix <<- y
+    theInverseMatrix <<- NULL
+  }
+  
+  #This function can be used to retrieve the set of numbers
+  get <- function() originalMatrix
+  
+  #Once calculated, this caches the mean for later
+  setinverse <- function(givenInverse) theInverseMatrix <<- givenInverse
+  
+  #Retrieve the pre-calculated mean (null if not calculated yet)
+  getinverse <- function() theInverseMatrix
+  
+  #Return the four functions, packaged as a list of four elements
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(MagicFunctionList, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  #Try retrieving a pre-calulated inverted matrix
+  PreCalculatedMatrixInverse <- MagicFunctionList$getinverse()
+  
+  #If it's not null then return it, you're done!
+  if(!is.null(PreCalculatedMatrixInverse)) {
+    message("getting cached data")
+    return(PreCalculatedMatrixInverse)
+  }
+  
+  #If we got here, it must have been null.  Need to calculate it...
+  #Grab the Matrix
+  newMatrix <- MagicFunctionList$get()
+  
+  #Calculate the inverted matrix
+  newMatrixInverse <- solve(newMatrix, ...)
+  
+  #Store it for later
+  MagicFunctionList$setinverse(newMatrixInverse)
+  
+  #Return the newly calculated inverted matrix
+  newMatrixInverse 
 }
